5656893a40661c20bc0faab2b5a9015737eceadb
Hammas <hammasshahid11@gmail.com>
2026-02-01 22:11:05 +0500

    setup tanstack router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 function App() {
-  return <div className="m-2 bg-green-100 text-blue-500">Home</div>;
+  return <div className="m-2 bg-green-100 text-blue-500">Home</div>
 }
 
-export default App;
+export default App
