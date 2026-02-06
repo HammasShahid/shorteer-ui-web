dee8035fe367b7634fcdf9e72f9a6bf9de8943bc
Hammas <hammasshahid11@gmail.com>
2026-02-06 19:15:08 +0500

    add route guard on auth routes

--- src/routes/(auth)/route.tsx
+++ src/routes/(auth)/route.tsx
@@ -1,15 +1,22 @@
-import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { createFileRoute, Outlet, redirect } from "@tanstack/react-router";
 import AuthNavbar from "@/components/navbars/AuthNavbar.tsx";
+import { useAuthStore } from "@/lib/api/auth.store.ts";
 
 export const Route = createFileRoute("/(auth)")({
   component: AuthLayout,
+  beforeLoad: () => {
+    const { isAuthenticated } = useAuthStore.getState();
+    if (isAuthenticated) {
+      throw redirect({ to: "/dashboard" });
+    }
+  },
 });
 
 function AuthLayout() {
   return (
     <>
       <AuthNavbar />
-      <main className="bg-muted min-h-[calc(100dvh-4rem)] grid place-items-center">
+      <main className="bg-muted grid min-h-[calc(100dvh-4rem)] place-items-center">
         <Outlet />
       </main>
     </>
